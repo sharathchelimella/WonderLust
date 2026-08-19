6f38627073f750f6d174721066163c6cf339609e
sharathchelimella <sharathkumarchelimella@gmail.com>
2026-08-19 22:48:42 +0530

    done with the image upload

--- routes/listing.js
+++ routes/listing.js
@@ -7,7 +7,8 @@ const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../Controllers/listing.js");
 const multer = require("multer");
-const upload = multer({dest: 'uploads/'});
+const {storage}  = require("./cloudConfig.js");
+const upload = multer({storage});
 
 // Create Route
 router.get("/new", isLoggedIn, listingController.rendernewform);
